6cf9d9b8811672fed4bac2b813dd7e51b1c01984
Sasank Madineni <sasank1020@gmail.com>
2024-03-15 01:01:22 -0700

    started updating new level file organization

--- LevelGen/level.py
+++ LevelGen/level.py
@@ -4,12 +4,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-from enum import Enum
-
-def block_type(Enum):
-    
-    pass
-
 class Level():
     
     def __init__(self, shape:tuple = (10, 10)):
@@ -17,13 +11,22 @@ class Level():
         self.layout = [[]]
         pass
 
-    def load_level(self, level_f:str):
+    def load_level(self, level_f:str) -> Level:
+        # Load in a level from a level file
         level_contents = open(level_f, 'r').readlines()
         for line in level_contents:
             # Read through level file line by line and construct a Level Object from it
             # print(line) # DEBUG
-            if(line[0] == )
+            if(len(line) == 0):
+                continue
+            line_arr = line.split(' ')
+            print(line_arr[0])
             pass
+        return self.Level
+    
+    def save_level(self, level_f:str):
+        # save this level to <level_f> file
+        
         pass
 
 
