224428d813ad30739d667341189018bb21d558e0
Sasank Madineni <sasank1020@gmail.com>
2024-04-16 18:31:04 -0700

    small update to block placer

--- Scripts/PCG/BlockPlacer.cs
+++ Scripts/PCG/BlockPlacer.cs
@@ -13,17 +13,23 @@ public partial class BlockPlacer : Node2D
 	private TileMap tilemap;
 	private Player player;
 	private string tilemap_path = "/root/world/Tilemap";
-	private const int BLOCK_SIZE = 16; // size of each block
-
+	private const int BLOCK_SIZE = 12; // size of each block
+	private Godot.Vector2 curr_offset = Godot.Vector2.Zero; // offset for the next room
 	private bool is_unix = true;
 
 	// Prefabs Dictionary
-	private Dictionary<string, PackedScene> block_dict;
+	private Dictionary<Object, PackedScene> block_dict;
+
+	// Enums
+	enum RoomPart { Left, Middle, Right };
+	enum Object { Block, Player, Spikeball, Enemy, FlyingEnemy, Door, Key, Coin, Checkpoint };
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ReloadBlockDictionary();
+
+		tilemap = GetNode<TileMap>("/root/World/Tilemap");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,19 +39,19 @@ public partial class BlockPlacer : Node2D
 	}
 
 	private void ReloadBlockDictionary() {
-		string[] block_type_list = {"player", "spikeball", "enemy", "flying_enemy", "door", "key", "coin", "checkpoint"};
+		// string[] block_type_list = {"player", "spikeball", "enemy", "flying_enemy", "door", "key", "coin", "checkpoint"};
 
-		UpdateBlockDict("player", "res://Prefabs/Player.tscn");
-		UpdateBlockDict("spikeball", "res://Prefabs/Objects/Spikeball.tscn");
-		UpdateBlockDict("enemy", "res://Prefabs/Glorp.tscn");
-		UpdateBlockDict("door", "res://Prefabs/Objects/door.tscn");
-		UpdateBlockDict("key", "res://Prefabs/Objects/key.tscn");
-		UpdateBlockDict("coin", "res://Prefabs/Objects/coin.tscn");
-		UpdateBlockDict("checkpoint", "res://Prefabs/Objects/checkpoint.tscn");
+		UpdateBlockDict(Object.Player, "res://Prefabs/Player.tscn");
+		UpdateBlockDict(Object.Spikeball, "res://Prefabs/Objects/Spikeball.tscn");
+		UpdateBlockDict(Object.Enemy, "res://Prefabs/Glorp.tscn");
+		UpdateBlockDict(Object.Door, "res://Prefabs/Objects/door.tscn");
+		UpdateBlockDict(Object.Key, "res://Prefabs/Objects/key.tscn");
+		UpdateBlockDict(Object.Coin, "res://Prefabs/Objects/coin.tscn");
+		UpdateBlockDict(Object.Checkpoint, "res://Prefabs/Objects/checkpoint.tscn");
 		
 	}
 
-	private void UpdateBlockDict(string key, string val_path) {
+	private void UpdateBlockDict(Object key, string val_path) {
 		if(block_dict.ContainsKey(key)) {
 			block_dict[key] = GD.Load<PackedScene>(val_path);
 		}
@@ -64,7 +70,9 @@ public partial class BlockPlacer : Node2D
 
 	}
 
-	public void PlaceBlock(string block_name, Godot.Vector2 pos) {
+	private void PlaceObject(Object obj_name, Godot.Vector2 pos) {
+		//Place an object of type <obj_name> at position <pos>
+		var obj = block_dict[obj_name].Instantiate();
 		
 	}
 }
