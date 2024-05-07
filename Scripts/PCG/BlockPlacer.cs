e4c0c3778b7854987279060dfeee2a3245297a8f
Sasank Madineni <sasank1020@gmail.com>
2024-05-06 19:20:21 -0700

    added a basic parallax background

--- Scripts/PCG/BlockPlacer.cs
+++ Scripts/PCG/BlockPlacer.cs
@@ -157,8 +157,8 @@ public partial class BlockPlacer : Area2D
 		// select a random amount of parts for the level
 		if(num_parts == -1) {
 			// select the minimum amount of rooms based on the current difficulty settin
-			int min_parts = (int) Math.Floor(difficulty * 0.33) + 1;
-			int max_parts = (int) Math.Floor(difficulty * 0.5) + 3;
+			int min_parts = 3;
+			int max_parts = (int)Math.Floor(difficulty/3) + 3;
 			num_parts_in_room = (int)random.Next(min_parts, max_parts+1); 
 		} 
 		else { 
