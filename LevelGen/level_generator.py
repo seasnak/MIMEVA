b0fca58061b0731799cf02ca05c71767bc72a2c8
Sasank Madineni <sasank1020@gmail.com>
2024-09-24 20:12:22 -0700

    started perlin noise implementation

--- LevelGen/level_generator.py
+++ LevelGen/level_generator.py
@@ -1,19 +1,30 @@
 import os
 import sys
-
 import random
-import perlin_numpy
+
+import matplotlib.pyplot as plt
 
 from level import Level
+from perlin_noise import PerlinNoise
+
 
 # File containing various algorithms to manipulate existing levels + generate levels
 
-def perlin_noise_level_manip(lev: Level):
+def perlin_noise_gen_background(lev: Level=None) -> Level:
     # manipulates a level using perlin noise
-    perlin_noise = perlin_numpy.generate_perlin_noise_2d((10, 10), (1, 1), (False, False))    
+    out_level = Level()
+    # perlin_noise = perlin_numpy.generate_perlin_noise_2d((10, 10), (1, 1), (False, False))    
     
+    perlin_noise = PerlinNoise(octaves=10)
+    xpix, ypix = 20, 20
+
+    # DEBUG: display perlin noise graph
+    # pic = [[perlin_noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
+    # plt.imshow(pic, cmap='gray')
+    # plt.show()
+
     pass
 
 if __name__ == "__main__":
-
+    perlin_noise_gen_background()
     sys.exit()
