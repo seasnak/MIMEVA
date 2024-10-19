6833942363c0b5df49ee654ed625bcbe8ce02555
Sasank Madineni <sasank1020@gmail.com>
2024-10-18 18:42:56 -0700

    fixed flag

--- Scripts/Objects/Flag.cs
+++ Scripts/Objects/Flag.cs
@@ -4,23 +4,31 @@ using System;
 
 public partial class Flag : Area2D
 {
-    [Export] private string final_scene_path;
+    [Export] private string final_scene_path = "";
 
     public override void _Ready() {
-        AreaEntered += OnAreaEntered;
+        BodyEntered += OnBodyEntered;
     }
 
     public override void _Process(double delta) {
 
     }
 
-    private void OnAreaEntered(Node2D other) {
+    private void OnBodyEntered(Node2D other) {
         if(other == null) { return; }
-
+        
         if(other is Player) {
             GD.Print("Player Finished Level!");
-            GetTree().ChangeSceneToFile(final_scene_path);
+
+            try {
+                GetTree().ChangeSceneToFile(final_scene_path);
+            }
+            catch {
+                GD.Print($"Error: Could not load scene \"{final_scene_path}\"");
+                throw;
+            }
         }
+
 
     }
 
