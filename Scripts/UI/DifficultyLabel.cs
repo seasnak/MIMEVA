a6717d42f5494b55cbb41185db9876614fb19a52
Sasank Madineni <sasank1020@gmail.com>
2024-11-18 16:41:19 -0800

    small changes to text display

--- Scripts/UI/DifficultyLabel.cs
+++ Scripts/UI/DifficultyLabel.cs
@@ -4,20 +4,32 @@ using System;
 
 namespace Mimeva.UI;
 
-public partial class DifficultyLabel : RichTextLabel {
+public partial class DifficultyLabel : RichTextLabel
+{
 
-	Player player;	
+    Player player;
+    /*[Export] FontFile font_f;*/
 
-	public override void _Ready() {
-    this.Text = "Difficulty:   ";
-    
-    // Adjust Appearance
-    this.Position = new(10, 20); 
-    this.Size = new(100, 100);
-    this.ScrollActive = false;
-	}
+    public override void _Ready()
+    {
+        this.Text = "Difficulty:   ";
 
-	public override void _Process(double delta) {
-		this.Text = $"Current Difficulty: {LevelGenVariables.LevelDifficulty}";
-	}
+        // Load font
+        /*Font f = new Godot.Font();*/
+        /*f.Theme.FontFile = font_f;*/
+
+
+        // Adjust Appearance
+        this.Position = new(10, 40);
+        this.Size = new(100, 100);
+        this.ScrollActive = false;
+        /*this.PushFontSize(20);*/
+        /*this.Theme.Set("normal_font", font_f);*/
+
+    }
+
+    public override void _Process(double delta)
+    {
+        this.Text = $"Difficulty: {LevelGenVariables.LevelDifficulty}";
+    }
 }
