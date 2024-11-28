e52e6af38e068cb0708e642a3d4a7618e638996f
Sasank Madineni <sasank1020@gmail.com>
2024-11-28 14:32:44 -0800

    started working on stats controller scritp

--- Scripts/UI/StatsController.cs
+++ Scripts/UI/StatsController.cs
@@ -1,15 +1,22 @@
 using Godot;
-using System;
+/*using System;*/
 
 public partial class StatsController : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    [Export] private Label death_label; // label containing death count
+    [Export] private Label coin_label; // label containing number of coins collected
+    [Export] private Label score_label; // label containing player's score at end of level
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        death_label = GetNode<Label>("Label");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+
+    }
 }
