ef75a2567124c67ffb77841509ab6197afa64073
Sasank Madineni <sasank1020@gmail.com>
2024-11-28 17:28:11 -0800

    added new death loc sprite

--- Scripts/Objects/Coin.cs
+++ Scripts/Objects/Coin.cs
@@ -1,31 +1,33 @@
 using Godot;
-using System;
-using System.Collections;
+/*using System;*/
+/*using System.Collections;*/
 
 namespace Mimeva;
 public partial class Coin : Area2D
 {
 
-	private AnimatedSprite2D sprite;
-	public override void _Ready() 
-	{
-		BodyEntered += OnBodyEntered;
+    private AnimatedSprite2D sprite;
+    public override void _Ready()
+    {
+        BodyEntered += OnBodyEntered;
 
-		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		sprite.SpeedScale = 2;
-		sprite.Play("idle");
-	}
+        sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        sprite.SpeedScale = 2;
+        sprite.ZIndex = 2;
+        sprite.Play("idle");
+    }
 
     public override void _Process(double delta)
     {
-        
+
     }
 
-	private void OnBodyEntered(Node body) {
-		if(body == null || body is not Player) { return; }
+    private void OnBodyEntered(Node body)
+    {
+        if (body == null || body is not Player) { return; }
 
-		((Player)body).AddCurrency(1);
-		QueueFree();
-	}
+        ((Player)body).AddCurrency(1);
+        QueueFree();
+    }
 
 }
