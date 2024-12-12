924473dbc29d8677055f626aa00f4647f42fa17d
Sasank Madineni <sasank1020@gmail.com>
2024-12-11 20:37:47 -0800

    minor changes

---
+++ Scripts/PCG/LevelGen.cs
@@ -0,0 +1,31 @@
+using System;
+using Godot;
+
+using Mimeva;
+namespace Mimeva.PCG;
+
+public class LevelGen : Node2D
+{
+
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+
+    public void GenerateNewLevel(int difficulty, (int, int) shape)
+    {
+
+    }
+
+    public void ModifyExistingLevel(int difficulty, string fname)
+    {
+
+    }
+
+
+}
