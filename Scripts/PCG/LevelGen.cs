b7499670641c27043d7f089dc5d96283a2a39e6d
Sasank Madineni <sasank1020@gmail.com>
2024-12-18 02:40:22 -0800

    started working on level modifications

--- Scripts/PCG/LevelGen.cs
+++ Scripts/PCG/LevelGen.cs
@@ -17,14 +17,23 @@ public class LevelGen : Node2D
 
     }
 
-    public void GenerateNewLevel(int difficulty, (int, int) shape)
+    public void GenerateNewLevel(int difficulty = 5, Vector2? shape = null)
     {
+        if (shape == null) { shape = new(10, 10); }
 
     }
 
-    public void ModifyExistingLevel(int difficulty, string fname)
+    public void ModifyExistingLevel(int target_difficulty, string fname)
     {
-
+        char start_difficulty = fname[0]; // difficulty of level from file --> E = 3, M = 5, H = 8
+        int diff_change = 0;
+        switch (start_difficulty)
+        {
+            case 'E': diff_change = target_difficulty - 3; return;
+            case 'M': diff_change = target_difficulty - 5; return;
+            case 'H': diff_change = target_difficulty - 8; return;
+            default: diff_change = 0; return;
+        };
     }
 
 
