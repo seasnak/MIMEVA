31207e83f7be2cbf899a4a2a89eea12a7a1e44ab
Sasank Madineni <sasank1020@gmail.com>
2025-01-05 19:13:04 -0800

    minor change, added new function

--- Scripts/PCG/LevelGen.cs
+++ Scripts/PCG/LevelGen.cs
@@ -7,39 +7,43 @@ namespace Mimeva.PCG;
 public partial class LevelGen : Node2D
 {
 
-	public override void _Ready()
-	{
+    public override void _Ready()
+    {
 
-	}
+    }
 
-	public override void _Process(double delta)
-	{
+    public override void _Process(double delta)
+    {
 
-	}
+    }
 
-	public void GenerateNewLevel(int difficulty = 5, Vector2? shape = null)
-	{
-		if (shape == null) { shape = new(10, 10); }
+    public void GenerateNewLevel(int difficulty = 5, Vector2? shape = null)
+    {
+        if (shape == null) { shape = new(10, 10); }
 
-	}
+    }
 
-	public void ModifyExistingLevel(int target_difficulty, string fname)
-	{
-		char start_difficulty = fname[0]; // difficulty of level from file --> E = 3, M = 5, H = 8
-		int diff_change = 0;
-		switch (start_difficulty)
-		{
-			case 'E': diff_change = target_difficulty - 3; return;
-			case 'M': diff_change = target_difficulty - 5; return;
-			case 'H': diff_change = target_difficulty - 8; return;
-			default: diff_change = 0; return;
-		};
-	}
+    public void ModifyExistingLevel(int target_difficulty, string fname)
+    {
+        char start_difficulty = fname[0]; // difficulty of level from file --> E = 3, M = 5, H = 8
+        int diff_change = 0;
+        switch (start_difficulty)
+        {
+            case 'E': diff_change = target_difficulty - 3; return;
+            case 'M': diff_change = target_difficulty - 5; return;
+            case 'H': diff_change = target_difficulty - 8; return;
+            default: diff_change = 0; return;
+        };
+    }
 
-	public void ModifyPart()
-	{
+    public void ModifyPart()
+    {
 
-	}
+    }
 
+    public void GenerateRhythm(int num_parts = 10, int difficulty = 5)
+    {
+
+    }
 
 }
